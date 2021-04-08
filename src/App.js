bfab9ae02e7203bf282480625b7a8c1db69de59a
John Trujillo <amriikk@gmail.com>
2021-04-08 03:05:42 -0700

    update json file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 // import { FaAngleDoubleRight } from 'react-icons/fa';
 import exp from './exp.json';
+import data from './data'
 import './App.css';
+
+const url = 'https://course-api.com/react-tabs-project';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [jobs, setJobs] = useState([]);
+  const [jobs, setJobs] = useState([data]);
   const [value, setValue] = useState(0);
 
   const fetchJobs = async () => {
@@ -32,6 +35,15 @@ function App() {
       <section className='section'>
         <div className= 'title'>
           <h2>Experience</h2>
+          <div className='underline'></div> 
+        </div>
+        <div className='jobs-center'>
+          {/* btn-container */}
+          {/* job info */}
+          <article className='job-info'>
+            <h3>{ title }</h3>
+            <h4>{ company }</h4>
+          </article>
         </div>
       </section>
     </div>
