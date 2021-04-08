ca26164ecd0163a4ffe0d3adbf754679766df89a
John Trujillo <amriikk@gmail.com>
2021-04-08 01:36:43 -0700

    set up fetch function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+import jobs from 'data.js';
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(jobs);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  }
+
+  useEffect( (loading) => {
+    fetchJobs();
+  }, []);
+
+  if(loading){
+    return <section className='section-loading'> 
+      <h1>loading...</h1>
+    </section>
+  }
+
   return (
     <div className="App">
       <h2>Experience Tabs</h2>
