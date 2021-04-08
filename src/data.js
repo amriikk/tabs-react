cf30fbb8435f47bee1fb1693af3c290a412eff84
John Trujillo <amriikk@gmail.com>
2021-04-08 00:48:48 -0700

    update app.js

--- src/data.js
+++ src/data.js
@@ -2,6 +2,7 @@ exp = [
     {   
         id: 01,
         order: 1,
+        company: 'JoTr.io',
         title: 'Lead Software Engineer',
         dates: 'October 2020 - November 2020',
         duties: [
@@ -9,50 +10,58 @@ exp = [
             'Consequat lumbersexual franzen portland, nostrud gochujang ethical edison bulb vape drinking vinegar hexagon scenester forage waistcoat.', 
             'Messenger bag tempor organic live-edge. Neutra reprehenderit sed flannel, iceland pork belly migas YOLO consequat tote bag gochujang lorem dolor sint.', 
             'Health goth swag poutine, consequat fingerstache sriracha keytar slow-carb in.'
-        ]
+        ],
     },
     {
         id: 02,
         order: 2,
+        company: 'RobertHalf',
         title: 'Field Support Technician',
         dates: 'October 2020 - November 2020',
         duties: [
             'Cardigan cold-pressed hella iceland, elit forage laborum photo booth magna truffaut.', 
             'Consequat lumbersexual franzen portland, nostrud gochujang ethical edison bulb vape drinking vinegar hexagon scenester forage waistcoat.', 
             'Messenger bag tempor organic live-edge. Neutra reprehenderit sed flannel, iceland pork belly migas YOLO consequat tote bag gochujang lorem dolor sint.', 
-            'Health goth swag poutine, consequat fingerstache sriracha keytar slow-carb in.'        ]
+            'Health goth swag poutine, consequat fingerstache sriracha keytar slow-carb in.'        
+        ]
     },
     {
         id: 03,
         order: 3,
+        company: 'Boost Mobile',
         title: 'Sales Manager',
         dates: 'October 2018 - August 2019',
         duties: [
             'Cardigan cold-pressed hella iceland, elit forage laborum photo booth magna truffaut.', 
             'Consequat lumbersexual franzen portland, nostrud gochujang ethical edison bulb vape drinking vinegar hexagon scenester forage waistcoat.', 
             'Messenger bag tempor organic live-edge. Neutra reprehenderit sed flannel, iceland pork belly migas YOLO consequat tote bag gochujang lorem dolor sint.', 
-            'Health goth swag poutine, consequat fingerstache sriracha keytar slow-carb in.'        ]
+            'Health goth swag poutine, consequat fingerstache sriracha keytar slow-carb in.'        
+        ]
     },
     {
         id: 04,
         order: 4,
+        company: 'Apple',
         title: 'Visual Specialist',
         dates: 'May 2014 - November 2016',
         duties: [
             'Cardigan cold-pressed hella iceland, elit forage laborum photo booth magna truffaut.', 
             'Consequat lumbersexual franzen portland, nostrud gochujang ethical edison bulb vape drinking vinegar hexagon scenester forage waistcoat.', 
             'Messenger bag tempor organic live-edge. Neutra reprehenderit sed flannel, iceland pork belly migas YOLO consequat tote bag gochujang lorem dolor sint.', 
-            'Health goth swag poutine, consequat fingerstache sriracha keytar slow-carb in.'        ]
+            'Health goth swag poutine, consequat fingerstache sriracha keytar slow-carb in.'        
+        ]
     },
     {
         id: 05,
         order: 5,
+        company: 'CorePower Yoga',
         title: 'Studio Experience Team',
         dates: 'January 2019 - March 2020',
         duties: [
             'Cardigan cold-pressed hella iceland, elit forage laborum photo booth magna truffaut.', 
             'Consequat lumbersexual franzen portland, nostrud gochujang ethical edison bulb vape drinking vinegar hexagon scenester forage waistcoat.', 
             'Messenger bag tempor organic live-edge. Neutra reprehenderit sed flannel, iceland pork belly migas YOLO consequat tote bag gochujang lorem dolor sint.', 
-            'Health goth swag poutine, consequat fingerstache sriracha keytar slow-carb in.'        ]
+            'Health goth swag poutine, consequat fingerstache sriracha keytar slow-carb in.'        
+        ]
     }
 ]
