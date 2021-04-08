ad0484fd0ff7576f4d5e07c11e4a296e839656b4
John Trujillo <amriikk@gmail.com>
2021-04-08 00:40:54 -0700

    seed data

---
+++ src/data.js
@@ -0,0 +1,47 @@
+exp = [
+    {   
+        id: 01,
+        order: 1,
+        title: 'Lead Software Engineer',
+        dates: 'October 2020 - November 2020',
+        duties: [
+            'Cardigan cold-pressed hella iceland, elit forage laborum photo booth magna truffaut.', 'Consequat lumbersexual franzen portland, nostrud gochujang ethical edison bulb vape drinking vinegar hexagon scenester forage waistcoat.', 'Messenger bag tempor organic live-edge. Neutra reprehenderit sed flannel, iceland pork belly migas YOLO consequat tote bag gochujang lorem dolor sint.', 'Health goth swag poutine, consequat fingerstache sriracha keytar slow-carb in.'
+        ]
+    },
+    {
+        id: 02,
+        order: 2,
+        title: 'Field Support Technician',
+        dates: 'October 2020 - November 2020',
+        duties: [
+            'Cardigan cold-pressed hella iceland, elit forage laborum photo booth magna truffaut.', 'Consequat lumbersexual franzen portland, nostrud gochujang ethical edison bulb vape drinking vinegar hexagon scenester forage waistcoat.', 'Messenger bag tempor organic live-edge. Neutra reprehenderit sed flannel, iceland pork belly migas YOLO consequat tote bag gochujang lorem dolor sint.', 'Health goth swag poutine, consequat fingerstache sriracha keytar slow-carb in.'
+        ]
+    },
+    {
+        id: 03,
+        order: 3,
+        title: 'Sales Manager',
+        dates: 'October 2018 - August 2019',
+        duties: [
+            'Cardigan cold-pressed hella iceland, elit forage laborum photo booth magna truffaut.', 'Consequat lumbersexual franzen portland, nostrud gochujang ethical edison bulb vape drinking vinegar hexagon scenester forage waistcoat.', 'Messenger bag tempor organic live-edge. Neutra reprehenderit sed flannel, iceland pork belly migas YOLO consequat tote bag gochujang lorem dolor sint.', 'Health goth swag poutine, consequat fingerstache sriracha keytar slow-carb in.'
+        ]
+    },
+    {
+        id: 04,
+        order: 4,
+        title: 'Visual Specialist',
+        dates: 'May 2014 - November 2016',
+        duties: [
+            'Cardigan cold-pressed hella iceland, elit forage laborum photo booth magna truffaut.', 'Consequat lumbersexual franzen portland, nostrud gochujang ethical edison bulb vape drinking vinegar hexagon scenester forage waistcoat.', 'Messenger bag tempor organic live-edge. Neutra reprehenderit sed flannel, iceland pork belly migas YOLO consequat tote bag gochujang lorem dolor sint.', 'Health goth swag poutine, consequat fingerstache sriracha keytar slow-carb in.'
+        ]
+    },
+    {
+        id: 05,
+        order: 5,
+        title: 'Studio Experience Team',
+        dates: 'January 2019 - March 2020',
+        duties: [
+            'Cardigan cold-pressed hella iceland, elit forage laborum photo booth magna truffaut.', 'Consequat lumbersexual franzen portland, nostrud gochujang ethical edison bulb vape drinking vinegar hexagon scenester forage waistcoat.', 'Messenger bag tempor organic live-edge. Neutra reprehenderit sed flannel, iceland pork belly migas YOLO consequat tote bag gochujang lorem dolor sint.', 'Health goth swag poutine, consequat fingerstache sriracha keytar slow-carb in.'
+        ]
+    }
+]
